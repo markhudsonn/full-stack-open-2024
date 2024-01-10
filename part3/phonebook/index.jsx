4457547cfd6df481c9babef0c1791f5776f43346
markhudsonn <markhudsonn4@gmail.com>
2024-01-10 11:51:47 +0000

    Phonebook morgan middleware

--- part3/phonebook/index.jsx
+++ part3/phonebook/index.jsx
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let people = [
     { 
