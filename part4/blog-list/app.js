ceb005b5154b3fa6e40ef4ffdc6a3b662cfc60d8
markhudsonn <markhudsonn4@gmail.com>
2024-01-19 11:08:43 +0000

    blog-list blog router

---
+++ part4/blog-list/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use('/api/blogs', blogsRouter)
+app.use(express.json())
+app.use(cors())
+
+module.exports = app
