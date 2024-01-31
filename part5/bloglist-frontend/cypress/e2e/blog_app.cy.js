1779c6b64bacbdfbafc785d01695a510f88e9bb1
markhudsonn <markhudsonn4@gmail.com>
2024-01-31 15:34:46 +0000

    Configured Cypress for blog app

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,11 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('login')
+  })
+
+})
