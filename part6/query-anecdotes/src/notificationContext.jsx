cd06729f24a89f5bc840dca6e6dd4152c4f1c3d2
markhudsonn <markhudsonn4@gmail.com>
2024-02-11 22:54:21 +0000

    query-anecdotes notification provider

---
+++ part6/query-anecdotes/src/notificationContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer, useContext } from 'react'
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.data
+    case 'CLEAR_NOTIFICATION':
+      return null
+    default:
+      return state
+  }
+}
+
+const NotificationContext = createContext()
+
+export const NotificationContextxProvider = ({ children }) => {
+  const [notification, dispatch] = useReducer(notificationReducer, null)
+
+  return (
+    <NotificationContext.Provider value={{ notification, dispatch }}>
+      {children}
+    </NotificationContext.Provider>
+  )
+}
+
+export const useNotificationValue = () => {
+  const notificationAndDispatch = useContext(NotificationContext)
+  return notificationAndDispatch[0]
+}
+
+export const useNotificationDispatch = () => {
+  const notificationAndDispatch = useContext(NotificationContext)
+  return notificationAndDispatch[1]
+}
+
+export default NotificationContext
